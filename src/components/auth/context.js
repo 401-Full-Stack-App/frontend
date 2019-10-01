efb9f2dd3d756e507d1ffc896316d88488b76a10
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-10-01 11:47:25 -0700

    save build progress

--- src/components/auth/context.js
+++ src/components/auth/context.js
@@ -4,10 +4,35 @@ import React from 'react';
 
 export const LoginContext = React.createContext(); 
 
+// what is our default state? 
 class LoginProvider extends React.Component {
   constructor(props) {
     super();
-    this.state = {}
+    this.state = {
+      // all the auth data we pass to children
+      loggedIn: false,
+      token: null,
+      user: {}, 
+      login: this.login,
+      logout: this.logout, 
+    }; 
+  }
+
+  // login
+  login() {
+    
+  }
+
+  // logout 
+
+  // validate token
+
+  // state management/handling 
+
+  // lifecycle component
+
+  componentDidMount() {
+    // when component is born, validate tokens, set cookies 
   }
 
   render() {
