0ee68401be8f67b977a1950cb8b0e89a7b8731f7
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-10-08 16:49:25 -0700

    clean up file directory

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,7 @@ import thunk from './middleware/thunk';
 // TODO: change this to your model re: forum 
 // import todos from './reducers/todos';
 
-const reducers = combineReducers({
-  todos,
-});
+
 
 const store = () => createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
